952105ffbdfa299f5411ce165f51213c51e40208
Denis Smirnov <w1png@yandex.ru>
2023-12-29 02:08:19 +0300

    Close modals on ESC key

--- static/javascript/utils.js
+++ static/javascript/utils.js
@@ -3,3 +3,18 @@ function ClearFormOnSubmit(event, form) {
 
   form.reset();
 }
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    const modals = document.querySelectorAll(".modal");
+    const modal_overlays = document.querySelectorAll(".modal-overlay");
+
+    modals.forEach((modal) => {
+      modal.dataset.open = false;
+    });
+
+    modal_overlays.forEach((modal_overlay) => {
+      modal_overlay.dataset.open = false;
+    });
+  }
+});
